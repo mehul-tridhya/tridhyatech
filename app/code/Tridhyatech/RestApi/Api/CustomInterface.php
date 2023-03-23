d2cd382cac6844ecb750761ec6119153acf00e8e
mehul-tridhya <mehul.tridhya@gmail.com>
2023-03-23 16:45:51 +0530

    Create Custom email template and Rest Api basic is done

---
+++ app/code/Tridhyatech/RestApi/Api/CustomInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tridhyatech\restApi\Api;
+
+interface CustomInterface
+
+{
+
+    /**
+     * GET for Post api
+     * @param string $value
+     * @return string
+     */
+
+ 
+
+    public function getData($value);
+
+}
---
+++ app/code/Tridhyatech/RestApi/Model/Api/Custom.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tridhyatech\RestApi\Model\Api;
+
+use Psr\Log\LoggerInterface;
+
+class Custom
+
+{
+
+    protected $logger;
+
+    public function __construct(
+
+        LoggerInterface $logger
+
+    ) {
+
+        $this->logger = $logger;
+    }
+
+
+
+    /**
+
+     * @inheritdoc
+
+     */
+
+    public function getData($value)
+
+    {
+
+        $response = ['success' => false];
+
+        try {
+            // Implement Your Code here
+            $response = ['success' => true, 'message' => $value];
+        } catch (\Exception $e) {
+
+            $response = ['success' => false, 'message' => $e->getMessage()];
+
+            $this->logger->info($e->getMessage());
+        }
+
+        $returnArray = json_encode($response);
+
+        return $returnArray;
+    }
+}
