a69e332b4f7bfae313610f59b830d36b11087d02
mehul-tridhya <mehul.tridhya@gmail.com>
2023-04-05 23:11:47 +0530

    change in dilevery note task

---
+++ app/code/Tridhyatech/EmailCustom/view/frontend/web/js/action/set-shipping-information-mixin.js
@@ -0,0 +1,30 @@
+/*jshint browser:true jquery:true*/
+/*global alert*/
+define([
+    'jquery',
+    'mage/utils/wrapper',
+    'Magento_Checkout/js/model/quote'
+], function ($, wrapper, quote) {
+    'use strict';
+
+    return function (setShippingInformationAction) {
+
+        return wrapper.wrap(setShippingInformationAction, function (originalAction) {
+            var shippingAddress = quote.shippingAddress();
+            var deliveryDate = $('[name="delivery_date"]').val();
+            var deliveryNote = $('[name="delivery_note"]').val();
+            if (shippingAddress.extensionAttributes) {
+                shippingAddress['extensionAttributes']['delivery_date'] = deliveryDate;
+                shippingAddress['extensionAttributes']['delivery_note'] = deliveryNote;
+            }else{
+                shippingAddress['extensionAttributes'] = {
+                    'delivery_date' : deliveryDate,
+                    'delivery_note': deliveryNote
+                }
+            }
+            console.log(shippingAddress.extensionAttributes);
+            // you can write here your code according to your requirement
+            return originalAction(); // it is returning the flow to original action
+        });
+    };
+});
